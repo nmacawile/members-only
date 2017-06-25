6c8ef7015715b9e7f41e202a27af9f5d5a69e79c
nmacawile <nmacawile@gmail.com>
2017-06-26 01:01:29 +0800

    Finish Static Pages views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
-module ApplicationHelper
+module ApplicationHelper	
+  def generate_title(title)
+  	app_name = "Members Only!"
+    return app_name if title.blank?
+    "#{title} | #{app_name}"
+  end
+
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,18 +2,21 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
+    assert_select "title", "Members Only!"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_url
     assert_response :success
+    assert_select "title", "About | Members Only!"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_url
     assert_response :success
+    assert_select "title", "Help | Members Only!"
   end
 
 end
